06a3e30887e7b4c3cf3309243845b433ab7e52ca
Guidotss <guidoolguin@hotmail.com>
2023-05-12 11:46:20 -0300

    Comenzando escritura y guardado de archivos en file system

--- src/components/snippets/SnippetsEditor.tsx
+++ src/components/snippets/SnippetsEditor.tsx
@@ -1,32 +1,45 @@
-import { useRef } from 'react';
-import { Editor } from '@monaco-editor/react';
+import { useEffect, useState } from "react";
+import { Editor } from "@monaco-editor/react";
 
+import { writeTextFile } from "@tauri-apps/api/fs";
+import { desktopDir } from "@tauri-apps/api/path";
+import { useSnippetsStore } from "../../store";
 
 export const SnippetsEditor = () => {
+  const { snippets } = useSnippetsStore();
+  console.log(snippets);
+  const [ code, setCode ] = useState<string | undefined>(""); 
 
-    const editorRef = useRef<any>(null);
 
-    const handleEditorDidMount = (editor: any, monaco: any) => {
-        editorRef.current = editor;
-        editorRef.current.focus();
+  useEffect(() => {
+    const saveFile = async () => {
+      const path = `${await desktopDir()}/snippets-app/snippets`;
+      const file = {
+        id: snippets[0].id,
+        title: snippets[0].title,
+        code: code,
+      }
+
+      await writeTextFile(`${path}/${snippets[0].id}.json`, `${JSON.stringify(file, null, 2)}`)
     }
-
-    
+    saveFile();
+  }, [code])
+      
 
   return (
     <div className="w-full h-screen bg-slate-800 overflow-y-hidden">
-        <div className="w-full h-full">
-            <Editor
-                height="100%"
-                width={window.innerWidth}
-                defaultLanguage="javascript"
-                theme='vs-dark'
-                options={{
-                    fontSize: 20,
-                }}
-                onMount={handleEditorDidMount}
-            />
-        </div>
+      <div className="w-full h-full">
+        <Editor
+          onChange={(value) => setCode(value)}
+          height="100%"
+          width={window.innerWidth}
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          options={{
+            fontSize: 20,
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
